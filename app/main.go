f859239e2e6ef158fb5c0bd2ca5a17822439de73
opipi406 <opipi.work@gmail.com>
2023-02-02 18:55:01 +0900

    add: rand test

--- app/main.go
+++ app/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println("hello world")
+	const COUNT = 1000000
+	var arr [6]int = [6]int{0, 0, 0, 0, 0, 0}
+
+	t := time.Now().UnixNano()
+	fmt.Println(t)
+	rand.Seed(t)
+
+	for i := 0; i < COUNT; i++ {
+		s := rand.Intn(6)
+		arr[s]++
+	}
+
+	for i := 0; i < len(arr); i++ {
+		avg := (float32(arr[i]) / float32(COUNT)) * 100.0
+		fmt.Printf("%d = %.2f％\n", i+1, avg)
+	}
+	fmt.Println(arr)
 }
